c702c520c7af2dcc0a2da91a2c8962ca77ec9862
610533 <610533@0338S-S-R0B6ZHM.sdcs.local>
2019-09-17 14:26:27 -0700

    Commit

--- src/extra/FourSquare.java
+++ src/extra/FourSquare.java
@@ -6,22 +6,22 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+Robot Tom =new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		Tom.setSpeed(100);
 		// 5. Set the pen width to 5
-
+		Tom.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+		for (int i= 0; i < 4; i++) {
 			// 7. Set the pen color to random
-	
+			Tom.setRandomPenColor();
 			// 1. Call the drawSquare() method
-	
+			
 			// 8. Turn the robot 90 degrees to the right
-
+			Tom.turn(90);
 	}
-
+	}
 	
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
